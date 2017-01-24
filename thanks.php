da08c2d02df8acc55a673fa1ad64610694ac92ef
jbyrnesshay <jmb464@g.harvard.edu>
2017-01-24 01:37:00 -0500

    first commit

---
+++ thanks.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>mail acknowlewdgement</title>
+<link href = "styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>thank you</h1>
+<p>thank you for your comments.  we'll be in touch if necessary.</p>
+</body>
+</html>
